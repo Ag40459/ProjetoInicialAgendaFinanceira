68bc76d135bc7358bb0765a7ea33b1abfa8a62a5
Agenor Torres <agenorpoker@gmail.com>
2022-09-22 20:36:31 -0300

    adicionado .env

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 app.use(route);
+const port = process.env.PORT || 3000
 
-
-app.listen(3000, () => { console.log(process.env.AGENOR); });
+app.listen(port, () => { console.log(process.env.AGENOR); });
